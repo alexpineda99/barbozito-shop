f1a71196d6f960a0f4828edc0bcf53522275fb23
alexpineda99 <74643984+alexpineda99@users.noreply.github.com>
2022-04-14 21:58:05 -0400

    Check session en app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Logout from "./components/Logout";
+import {useSelector} from "react-redux";
+
 
 function App() {
+  const checksession = useSelector(state=>state.user.isAuth);
+
+  useEffect(() => {
+    () => {
+      if (!checksession) {
+        window.localStorage.clear();
+      } else {
+        return null;
+      }
+    }
+  }, [])
+
   return (
     <Router>
       <Routes>
